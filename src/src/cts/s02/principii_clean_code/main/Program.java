0ff36ac4cb2d5e4e4851f1951446bb9be121fc33
MihaceaAlexandru <114437946+MihaceaAlexandru@users.noreply.github.com>
2023-03-01 10:18:46 +0200

    Repair clases with clean code principles

--- src/src/cts/s02/principii_clean_code/main/Program.java
+++ src/src/cts/s02/principii_clean_code/main/Program.java
@@ -3,17 +3,19 @@ package src.cts.s02.principii_clean_code.main;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import src.cts.s02.principii_clean_code.clase.Angajat;
-import src.cts.s02.principii_clean_code.clase.Utils;
+import src.cts.s02.principii_clean_code.clase.Aplicant;
+import src.cts.s02.principii_clean_code.clase.readers.AngajatReader;
+import src.cts.s02.principii_clean_code.clase.readers.AplicantReader;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			AplicantReader aplicantReader = new AngajatReader();
+			listaAngajati = aplicantReader.readAplicanti("angajati.txt");
+			for(Aplicant aplicat:listaAngajati)
+				System.out.println(aplicat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
